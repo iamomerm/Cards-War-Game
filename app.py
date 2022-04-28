11d23e1f4d25380f2142b70165e513d0b8b3d011
Omer Menashe <46810647+iamomerm@users.noreply.github.com>
2022-04-28 11:34:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ import itertools
 from termcolor import colored
 
 suits = ['Hearts', 'Clubs', 'Spades', 'Diamonds']
-cards = {'2': 2, '3': 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, 'Jack': 11, "Queen": 12, "King": 13, "Ace": 14}
+cards = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
 
 
 def valid_input():
     options = ['DRAW', 'EXIT']
     while True:
-        value = input("[Gamebot] Answer %s: " % options).upper()
+        value = input('[Gamebot] Answer %s: ' % options).upper()
         if value not in options:
             print('[Gamebot] Invalid Answer: %s (Accepted Answers: %s)' % (value, options))
         elif value == 'EXIT':
-            print("Exiting...\n")
+            print('Exiting...\n')
             return None
         else:
             return value
@@ -24,20 +24,20 @@ def draw(deck):
     deck.remove(computer_random_card)
     user_random_card = random.choice(deck)
     deck.remove(user_random_card)
-    print("[Gamebot] You Got: %s" % colored(user_random_card, 'green'))
-    print("[Gamebot] Computer Got: %s" % colored(computer_random_card, 'red'))
-    print("[Gamebot] Cards Remaining: %d" % len(deck))
+    print('[Gamebot] You Got: %s' % colored(user_random_card, 'green'))
+    print('[Gamebot] Computer Got: %s' % colored(computer_random_card, 'red'))
+    print('[Gamebot] Cards Remaining: %d' % len(deck))
     computer_random_card_value = cards[computer_random_card.split(' ')[0]]
     user_random_card = cards[user_random_card.split(' ')[0]]
     status = None
     if computer_random_card_value > user_random_card:
-        print("[Gamebot] You Lost!")
+        print('[Gamebot] You Lost!')
         status = -1
     elif computer_random_card_value < user_random_card:
-        print("[Gamebot] You Won!")
+        print('[Gamebot] You Won!')
         status = 1
     else:
-        print("[Gamebot] Tie")
+        print('[Gamebot] Tie')
         status = 0
     return [status, deck]
 
@@ -50,5 +50,5 @@ while deck:
         break
     status, deck = draw(deck)
     score += status
-    print("[Gamebot] Your Score: %d\n" % score)
-print("[Gamebot] GAME OVER! Your Score: %d" % score)
+    print('[Gamebot] Your Score: %d\n' % score)
+print('[Gamebot] GAME OVER! Your Score: %d' % score)
